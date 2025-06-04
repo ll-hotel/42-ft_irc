4fed594c7864ad75e732b62d79b1b8d8e30eb23f
gcros <guilhem.cros.38@gmail.com>
2025-06-04 03:23:43 +0200

    fix invalid read

--- src/command_who.cpp
+++ src/command_who.cpp
@@ -3,17 +3,22 @@
 
 void Server::commandWho(const Command &command, User &user) const
 {
-	if (command.args.size() < 1) {
-		this->errNeedMoreParams(user, command.name);
-		return;
-	}
-	bool op_only = false;
-	if (command.args[1] == "o")
-		op_only = true;
-	std::string mask = command.args[0];
 	std::vector<User *> user_list;
+	std::string mask;
+
+	bool op_only = false;
+	if (command.args.size() > 1 && command.args[1] == "o")
+		op_only = true;
+	if (command.args.size() == 0)
+		mask = "*";
+	else
+		mask = command.args[0];
+
 	std::set<size_t> chan_ops;
-	if (mask[0] == '#') {
+	if (mask == "*") {
+		user_list = this->m_users;
+	}
+	else if (mask[0] == '#') {
 		mask = strTolower(mask);
 		std::vector<Channel *>::const_iterator chan_it = this->getChannelByName(mask);
 		if (chan_it == this->m_channels.end()) {
@@ -41,13 +46,15 @@ void Server::commandWho(const Command &command, User &user) const
 	std::string msg = getReplyBase(RPL_WHOREPLY, user) + " " + mask + " ";
 	for (size_t i = 0; i < user_list.size(); i++) {
 		User *u = user_list[i];
-		std::string msg_end = u->username + " " + u->hostname + " " + u->servername + " " +
-							  u->nickname + " " + "H" + " ";
+		std::string msg_end =
+			u->username + " " + u->hostname + " " + u->servername + " " + u->nickname + " " + "H";
 		if (this->m_ops.find(u->id) != this->m_ops.end())
-			msg_end += "* ";
+			msg_end += "*";
+		else
+			msg_end += " ";
 		if (chan_ops.find(u->id) != chan_ops.end())
 			msg_end += "@ ";
-		msg_end += " :0 " + u->realname;
+		msg_end += ":0 " + u->realname;
 		user.send(msg + msg_end + "\n\r");
 	}
 	this->rplEndOfWho(user, mask);
