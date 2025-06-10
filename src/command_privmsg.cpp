cd1729fe939cb978062e014dc491a51f3468e99d
ll-hotel <ll-hotel@student.42lyon.fr>
2025-06-10 16:20:46 +0200

    feat: privmsg to multiple channels

--- src/command_privmsg.cpp
+++ src/command_privmsg.cpp
@@ -1,4 +1,5 @@
 #include "Server.hpp"
+#include "ft_split.hpp"
 #include "utils.hpp"
 
 void Server::commandPrivMsg(const Command &cmd, User &user) const
@@ -7,29 +8,34 @@ void Server::commandPrivMsg(const Command &cmd, User &user) const
 		this->errNoRecipient(user, cmd.name);
 		return;
 	}
-	const std::string dest = strTolower(cmd.args[0]);
+	const std::string targets_str = strTolower(cmd.args[0]);
 	if (cmd.args.size() < 2) {
 		this->errNoTextToSend(user);
 		return;
 	}
+	const std::vector<std::string> targets = ft_split(targets_str, ',');
 	const std::string &message = cmd.args[1];
-	{
-		std::vector<Channel *>::const_iterator chan_pos = this->getChannelByName(dest);
-		if (chan_pos != m_channels.end()) {
-			Channel &dest_chan = *(*chan_pos);
-			dest_chan.privmsg(user.id, ':' + user.clientName(m_hostname) + " PRIVMSG " +
-										   dest_chan.name() + " :" + message + "\r\n");
-			return;
+	for (std::vector<std::string>::const_iterator target_it = targets.begin();
+		 target_it != targets.end(); target_it++) {
+		const std::string &target = *target_it;
+		{
+			std::vector<Channel *>::const_iterator chan_pos = this->getChannelByName(target);
+			if (chan_pos != m_channels.end()) {
+				Channel &dest_chan = *(*chan_pos);
+				dest_chan.privmsg(user.id, ':' + user.clientName(m_hostname) + " PRIVMSG " +
+											   dest_chan.name() + " :" + message + "\r\n");
+				continue;
+			}
 		}
-	}
-	{
-		std::vector<User *>::const_iterator user_pos = this->getUserbyNickname(dest);
-		if (user_pos != m_users.end()) {
-			User &dest_user = *(*user_pos);
-			dest_user.send(':' + user.clientName(m_hostname) + " PRIVMSG " + dest_user.nickname +
-						   " :" + message + "\r\n");
-			return;
+		{
+			std::vector<User *>::const_iterator user_pos = this->getUserbyNickname(target);
+			if (user_pos != m_users.end()) {
+				User &dest_user = *(*user_pos);
+				dest_user.send(':' + user.clientName(m_hostname) + " PRIVMSG " +
+							   dest_user.nickname + " :" + message + "\r\n");
+				continue;
+			}
 		}
+		this->errNoSuchNick(user, targets_str);
 	}
-	this->errNoSuchNick(user, dest);
 }
